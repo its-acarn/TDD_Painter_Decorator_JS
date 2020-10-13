b1c93463172659dc07074ef5ddc1c0dfb0f2df15
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-10-13 17:16:15 +0100

    tests completed decorator constructor created in full and function to paint room

--- decorator.js
+++ decorator.js
@@ -8,11 +8,30 @@ Decorator.prototype.addStock = function(paint){
 
 Decorator.prototype.calculatePaintLitres = function() {
     let stockLitres = 0;
-    for (let i=0; i < this.stock.length; i++) {
-        stockLitres = stockLitres + this.stock[i].litres;
+    for (let paint of this.stock) {
+        stockLitres = stockLitres + paint.litres;
     };
     return stockLitres;
+    // for (let i=0; i < this.stock.length; i++) {
+    //     stockLitres = stockLitres + this.stock[i].litres;
+    // };
+    // return stockLitres;
 };
+
+Decorator.prototype.enoughPaint = function(room){
+    const totalLitres = this.calculatePaintLitres();
+    const roomArea = room.area 
+    if (totalLitres >= roomArea){
+        return true
+    }
+    return false
+}
+
+Decorator.prototype.paintRoom = function(room){
+    if (this.enoughPaint(room) === true){
+        room.painted = true
+    }
+}
 
 module.exports = Decorator;
 
--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -1,11 +1,13 @@
 const assert = require('assert');
 const Decorator = require('../decorator.js');
 const Paint = require('../paint.js');
+const Room = require('../room.js');
 
 describe('Decorator', function(){
     let decorator
     let paint_1
     let paint_2
+    let room 
     // creating var outside of testing for use throughout
 
     beforeEach(function(){
@@ -14,6 +16,7 @@ describe('Decorator', function(){
         paint_2 = new Paint (2, false);
         decorator.addStock(paint_1)
         decorator.addStock(paint_2)
+        room = new Room(10, false)
     })
 
     it('should have no stock', function(){
@@ -31,4 +34,16 @@ describe('Decorator', function(){
         const actual = decorator.calculatePaintLitres();
         assert.strictEqual(actual, 12);
     });
+
+    it ('should have enough paint for room', function(){
+        const actual = decorator.enoughPaint(room);
+        assert.strictEqual(actual, true)
+    })
+
+    it ('should paint room', function(){
+        decorator.paintRoom(room);
+        const actual = room.painted;
+        assert.strictEqual(actual, true)
+    })
+
 })
