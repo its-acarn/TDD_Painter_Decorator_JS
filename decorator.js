5157d555dfff3be20e20ebfe050d9f3b4f728387
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-10-13 16:57:17 +0100

    tidied up constructor and tested accordingly.

--- decorator.js
+++ decorator.js
@@ -6,16 +6,13 @@ Decorator.prototype.addStock = function(paint){
     this.stock.push(paint)
 }
 
-
-// Decorator.prototype.addStock = function() {
-//     let stockLitres = 0;
-//     for (let i=0; i < this.stock.length; i++) {
-//         stockLitres = stockLitres + this.stock[i].litres;
-//     };
-//     return stockLitres;
-// };
-
-
+Decorator.prototype.calculatePaintLitres = function() {
+    let stockLitres = 0;
+    for (let i=0; i < this.stock.length; i++) {
+        stockLitres = stockLitres + this.stock[i].litres;
+    };
+    return stockLitres;
+};
 
 module.exports = Decorator;
 
--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -3,21 +3,32 @@ const Decorator = require('../decorator.js');
 const Paint = require('../paint.js');
 
 describe('Decorator', function(){
+    let decorator
+    let paint_1
+    let paint_2
+    // creating var outside of testing for use throughout
+
     beforeEach(function(){
         decorator = new Decorator();
         paint_1 = new Paint (10, false);
         paint_2 = new Paint (2, false);
+        decorator.addStock(paint_1)
+        decorator.addStock(paint_2)
     })
 
     it('should have no stock', function(){
+        const decorator = new Decorator();
         const actual = decorator.stock;
         assert.deepStrictEqual(actual, []);
     })
     
     it('should add stock', function(){
-        decorator.addStock(paint_1)
-        decorator.addStock(paint_2)
         const actual = decorator.stock;
         assert.deepStrictEqual(actual, [paint_1, paint_2])
     })
+
+    it ('should calculate total paint litres', function() {
+        const actual = decorator.calculatePaintLitres();
+        assert.strictEqual(actual, 12);
+    });
 })
