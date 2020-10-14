eb6bf7118a4e06d9d40dc6c40b9f5aa5be1bb025
svvishjs <andreww92@live.com>
2020-10-14 09:57:20 +0100

    made reducePaint function when room painted

--- decorator.js
+++ decorator.js
@@ -1,38 +1,59 @@
-const Decorator = function(){
-    this.stock = [];
-}
-
-Decorator.prototype.addStock = function(paint){
-    this.stock.push(paint)
-}
-
-Decorator.prototype.calculatePaintLitres = function() {
-    let stockLitres = 0;
-    for (let paint of this.stock) {
-        stockLitres = stockLitres + paint.litres;
-    };
-    return stockLitres;
-    // for (let i=0; i < this.stock.length; i++) {
-    //     stockLitres = stockLitres + this.stock[i].litres;
-    // };
-    // return stockLitres;
+const Decorator = function () {
+	this.stock = [];
 };
 
-Decorator.prototype.enoughPaint = function(room){
-    const totalLitres = this.calculatePaintLitres();
-    const roomArea = room.area 
-    if (totalLitres >= roomArea){
-        return true
-    }
-    return false
-}
+Decorator.prototype.addStock = function (paint) {
+	this.stock.push(paint);
+};
 
-Decorator.prototype.paintRoom = function(room){
-    if (this.enoughPaint(room) === true){
-        room.painted = true
-    }
-}
+Decorator.prototype.calculatePaintLitres = function () {
+	let stockLitres = 0;
+	for (let paint of this.stock) {
+		stockLitres = stockLitres + paint.litres;
+	}
+	return stockLitres;
+	// for (let i=0; i < this.stock.length; i++) {
+	//     stockLitres = stockLitres + this.stock[i].litres;
+	// };
+	// return stockLitres;
+};
+
+Decorator.prototype.enoughPaint = function (room) {
+	const totalLitres = this.calculatePaintLitres();
+	const roomArea = room.area;
+	if (totalLitres >= roomArea) {
+		return true;
+	}
+	return false;
+};
+
+Decorator.prototype.paintRoom = function (room) {
+	if (this.enoughPaint(room) === true) {
+		room.painted = true;
+	}
+};
+
+Decorator.prototype.reducePaint = function (room) {
+	let paintUsed = room.area;
+
+	while (paintUsed > 0) {
+		for (let paint of this.stock) {
+			let paintValue;
+
+			if (paint.litres > paintUsed) {
+				paintValue = paintUsed;
+			} else {
+				paintValue = paint.litres;
+			}
+
+			paintUsed = paintUsed - paintValue;
+			paint.litres = paint.litres - paintValue;
+
+			if (paint.litres === 0) {
+				paint.empty = true;
+			}
+		}
+	}
+};
 
 module.exports = Decorator;
-
-
--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -3,47 +3,66 @@ const Decorator = require('../decorator.js');
 const Paint = require('../paint.js');
 const Room = require('../room.js');
 
-describe('Decorator', function(){
-    let decorator
-    let paint_1
-    let paint_2
-    let room 
-    // creating var outside of testing for use throughout
+describe('Decorator', function () {
+	let decorator;
+	let paint_1;
+	let paint_2;
+	let room;
+	// creating var outside of testing for use throughout
 
-    beforeEach(function(){
-        decorator = new Decorator();
-        paint_1 = new Paint (10, false);
-        paint_2 = new Paint (2, false);
-        decorator.addStock(paint_1)
-        decorator.addStock(paint_2)
-        room = new Room(10, false)
-    })
+	beforeEach(function () {
+		decorator = new Decorator();
+		paint_1 = new Paint(3, false);
+		paint_2 = new Paint(2, false);
+		paint_3 = new Paint(1, false);
+		paint_4 = new Paint(4, false);
+		paint_5 = new Paint(2, false);
+		paint_6 = new Paint(3, false);
+		decorator.addStock(paint_1);
+		decorator.addStock(paint_2);
+		decorator.addStock(paint_3);
+		decorator.addStock(paint_4);
+		decorator.addStock(paint_5);
+		decorator.addStock(paint_6);
+		room = new Room(12, false);
+	});
 
-    it('should have no stock', function(){
-        const decorator = new Decorator();
-        const actual = decorator.stock;
-        assert.deepStrictEqual(actual, []);
-    })
-    
-    it('should add stock', function(){
-        const actual = decorator.stock;
-        assert.deepStrictEqual(actual, [paint_1, paint_2])
-    })
+	it('should have no stock', function () {
+		const decorator = new Decorator();
+		const actual = decorator.stock;
+		assert.deepStrictEqual(actual, []);
+	});
 
-    it ('should calculate total paint litres', function() {
-        const actual = decorator.calculatePaintLitres();
-        assert.strictEqual(actual, 12);
-    });
+	// it('should add stock', function () {
+	// 	const actual = decorator.stock;
+	// 	assert.deepStrictEqual(actual, [paint_1, paint_2]);
+	// });
 
-    it ('should have enough paint for room', function(){
-        const actual = decorator.enoughPaint(room);
-        assert.strictEqual(actual, true)
-    })
+	it('should calculate total paint litres', function () {
+		const actual = decorator.calculatePaintLitres();
+		assert.strictEqual(actual, 15);
+	});
 
-    it ('should paint room', function(){
-        decorator.paintRoom(room);
-        const actual = room.painted;
-        assert.strictEqual(actual, true)
-    })
+	it('should have enough paint for room', function () {
+		const actual = decorator.enoughPaint(room);
+		assert.strictEqual(actual, true);
+	});
 
-})
+	it('should paint room', function () {
+		decorator.paintRoom(room);
+		const actual = room.painted;
+		assert.strictEqual(actual, true);
+	});
+
+	it('should decrease paint when painting a room', function () {
+		decorator.reducePaint(room, decorator.stock);
+		const actual = decorator.calculatePaintLitres();
+		assert.strictEqual(actual, 3);
+		console.log(paint_1);
+		console.log(paint_2);
+		console.log(paint_3);
+		console.log(paint_4);
+		console.log(paint_5);
+		console.log(paint_6);
+	});
+});
